380e1d71ba11abf3180c9b89f0c862594247f168
richard <richard@cardgate.com>
2019-12-05 15:53:31 +0100

    remove test code.

--- tests/GatewayTest.php
+++ tests/GatewayTest.php
@@ -10,10 +10,6 @@
  */
 namespace Omnipay\Cardgate;
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 use Omnipay\Omnipay;
 use Omnipay\Tests\GatewayTestCase;
 
