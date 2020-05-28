5ed4c93150b2c7783f34cbb35c82f3efdd9b109e
Mekaeil <maikel1370@gmail.com>
2020-05-28 15:49:23 +0430

    Update FetchIssuersRequest.php

--- src/Message/FetchIssuersRequest.php
+++ src/Message/FetchIssuersRequest.php
@@ -3,10 +3,10 @@
 /*
  * CardGate driver for Omnipay PHP payment processing library
  * https://www.cardgate.com/
- * 
+ *
  * Latest driver release:
  * https://github.com/cardgate/
- * 
+ *
  */
 
 namespace Omnipay\Cardgate\Message;
@@ -18,45 +18,45 @@ use Omnipay\Common\Message\ResponseInterface;
 
 /**
  * FetchIssuersRequest class - it fetches Issuers.
- * 
+ *
  * @author Martin Schipper martin@cardgate.com
  */
-class FetchIssuersRequest extends AbstractRequest 
-{    
-    protected $endpoint = '/rest/v1/ideal/issuers/';
-    
+class FetchIssuersRequest extends AbstractRequest
+{
+    protected $endpoint = '/rest/v1/curo/ideal/issuers/';
+
     /**
      * {@inheritdoc}
      */
-    public function getData() {
+    public function getData()
+    {
         return array();
     }
 
     /**
      * {@inheritdoc}
      */
-    public function sendData( $data ) 
+    public function sendData($data = null)
     {
-       
         try {
-			$response = $this->httpClient->request('GET', ($this->getUrl() . $this->endpoint), [
-                'Accept' => 'application/xml',
-				'Content-Type' => 'application/json',
-				'Authorization' => 'Basic '. base64_encode($this->getMerchantId() . ':' . $this->getApiKey()),
-			], http_build_query($data));
-			 
-			$httpResponse = simplexml_load_string($response->getBody()->getContents());
+            
+            $response = $this->httpClient->request('GET', ($this->getUrl() . $this->endpoint), [
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json',
+                'Authorization' => 'Basic ' . base64_encode($this->getMerchantId() . ':' . $this->getApiKey()),
+            ]);
+
+            $httpResponse = json_decode($response->getBody()->getContents(), true);
+
+        } catch (Exception $e) {
+            if ($this->getTestMode()) {
+                throw new InvalidResponseException('CardGate RESTful API gave : ' . $e->getMessage(), $e->getCode());
+            } else {
+                throw $e;
+            }
         }
-        catch (Exception $e) {
-			if ( $this->getTestMode() ) {
-				throw new InvalidResponseException('CardGate RESTful API gave : ' . $e->getMessage(),$e->getCode());
-			} 
-			else {
-				throw $e;
-			}
-        }
-        return $this->response = new FetchIssuersResponse ($this, $httpResponse->xml() );
-        
+        return $this->response = new FetchIssuersResponse ($this, $httpResponse);
+
     }
 
 }
